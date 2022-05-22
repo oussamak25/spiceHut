3bb293867a4b2746e7678047331ad7eca69234ba
oussamak25 <oussama.elkajjoui.smr@gmail.com>
2022-05-22 23:54:30 +0200

    a

--- src/pages/FrPrincipal.vue
+++ src/pages/FrPrincipal.vue
@@ -123,7 +123,7 @@
     </div>
     <div v-if="descripcionPlato">
       <UCPlatoDescripcion
-        :img="platoSelected.img"
+        :img="`../assets/img/prod/${platoSelected.img}`"
         :nombre="platoSelected.nombre"
         :precio="platoSelected.precio"
         :info-plato="platoSelected.descEs"
